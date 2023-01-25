43a863a108550fb9f809bd67483b7d155fff2098
bootcoding <bootcoding@gmail.com>
2023-01-25 10:23:08 +0530

    #Java-Project-Training - Java programs

---
+++ src/main/java/com/bootcoding/project/model/Customer.java
@@ -0,0 +1,60 @@
+package com.bootcoding.project.model;
+
+public class Customer {
+    private int id;
+    private String name;
+    private String deliveryAddress;
+    private String emailId;
+    private long phoneNumber;
+    private String city;
+
+    // Getter and Setter methods
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDeliveryAddress() {
+        return deliveryAddress;
+    }
+
+    public void setDeliveryAddress(String deliveryAddress) {
+        this.deliveryAddress = deliveryAddress;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
---
+++ src/main/java/com/bootcoding/project/utils/CustomerNameGenerator.java
@@ -0,0 +1,39 @@
+package com.bootcoding.project.utils;
+
+import java.util.Random;
+
+public class CustomerNameGenerator {
+    private static final String[] names = {
+            "Ramesh", "Suresh", "Ganesh", "Dinesh", "Mangesh", "Jayesh", "Pooja", "Shree", "Amrita", "Atul", "Sujal",
+            "Sakshi", "Namrata", "Shilpa", "Ashwini", "Nikhil", "Manjit", "Sodi", "Aryan", "Sumit", "Sarang"};
+
+    private static Random random = new Random();
+
+    public static String getName(){
+        int randomIndex = random.nextInt(names.length);
+        return names[randomIndex];
+    }
+
+    public static void main(String[] args) {
+        for(int i = 0; i < 10; i++){
+            System.out.println(CustomerNameGenerator.getName());
+        }
+    }
+
+    public int binarySearch(int[] a, int low, int high, int x){
+
+        if(low > high){
+            return -1;
+        }
+
+        int mid = low + (high-low) / 2;
+        if(a[mid] == x){
+            return mid;
+        }
+        if(a[mid] > x){
+            return binarySearch(a, low, mid-1, x);
+        }else{
+            return binarySearch(a, mid+1, high, x);
+        }
+    }
+}
