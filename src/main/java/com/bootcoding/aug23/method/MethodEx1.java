c1df5b91f2d6fdf6dab4d7b87b5c747066bbad54
bootcoding-developer <bootcoding.dev@gmail.com>
2023-09-13 09:12:04 +0530

    method ex

---
+++ src/main/java/com/bootcoding/aug23/method/MethodEx1.java
@@ -0,0 +1,21 @@
+package com.bootcoding.aug23.method;
+
+public class MethodEx1 {
+
+    // Method Definition
+    public static void print(){
+        String msg = "ANY TEXT";
+        System.out.println(msg);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("I am main() method");
+        // Method Calling
+        print();
+        System.out.println("I'm finished!");
+    }
+}
+// Method Return Type
+// Method Parameters
+// Order of Method Parameters
